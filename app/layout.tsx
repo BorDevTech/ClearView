ba7204bddbd6dc97649ee3217c8db3ce3a506381
Devell Robinson <bordevtech@icloud.com>
2025-08-05 19:48:57 +0000

    feat: Add clarity analytics script to the root layout for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Provider } from "../components/ui/provider";
 
@@ -14,6 +15,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="clarity-analytics" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "sq8dhiettj");`}
+        </Script>
+      </head>
       <body>
         <Provider>{children}</Provider>
       </body>
